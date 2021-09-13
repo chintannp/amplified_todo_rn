c181b1b8dd827508529359af5e605459736c1676
Chintan Patel <chintann@amazon.com>
2021-09-13 18:12:43 -0400

    added flex to the root App Component

--- App.js
+++ App.js
@@ -20,8 +20,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
