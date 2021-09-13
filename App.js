2a639253640b080a3ba9a57868559416afb09d75
Chintan Patel <chintann@amazon.com>
2021-09-13 18:45:37 -0400

    removed additional unrequired css properties from root component

--- App.js
+++ App.js
@@ -21,8 +21,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
   },
 });
