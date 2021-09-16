77d541a9bf4df534ba80a49654f8663538733700
Chintan Patel <chintann@amazon.com>
2021-09-16 13:54:31 -0400

    onQuery, subscription and prettierrc changes

--- src/Header.js
+++ src/Header.js
@@ -3,24 +3,24 @@ import { StyleSheet, Text, View } from 'react-native';
 
 
 export default function Header() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Todo App</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Todo App</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        width: "100%",
-        paddingVertical: 30,
-        backgroundColor: '#4696ec',
-    },
-    title: {
-        marginTop: 20,
-        textAlign: "center",
-        color: "white",
-        fontSize: 24,
-        fontWeight: "600"
-    }
+  container: {
+    width: "100%",
+    paddingVertical: 30,
+    backgroundColor: '#4696ec',
+  },
+  title: {
+    marginTop: 20,
+    textAlign: "center",
+    color: "white",
+    fontSize: 24,
+    fontWeight: "600"
+  }
 });
--- src/Home.js
+++ src/Home.js
@@ -5,107 +5,106 @@ import { Todo } from './models';
 
 export default function Home() {
 
-    const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
 
-    let subscription;
+  useEffect(() => {
+    const onQuery = async () => {
+      const todos_list = await DataStore.query(Todo);
+      setTodos(todos_list);
+    };
 
-    useEffect(() => {
-        onQuery = async () => {
-            const todos_list = await DataStore.query(Todo);
-            setTodos(todos_list);
-        };
-        onQuery();
-        if (typeof subscription === 'undefined') {
-            subscription = DataStore.observe(Todo).subscribe((msg) => {
-                onQuery();
-            });
-        }
+    //load todos on first render 
+    onQuery();
 
-        return function cleanup() {
-            subscription.unsubscribe();
-        }
-    }, []);
+    const subscription = DataStore.observe(Todo).subscribe((msg) => {
+      onQuery();
+    });
 
-    async function setComplete(updateValue, todo) {
-        await DataStore.save(
-            Todo.copyOf(todo, updated => {
-                updated.isComplete = updateValue
-            })
-        );
+    return function cleanup() {
+      subscription.unsubscribe();
     }
+  }, []);
 
-    const todoItem = ({ item }) => (
-        <View style={styles.todoContainer}>
-            <View>
-                <Text style={styles.todoHeading}>{item.name}</Text>
-                <Text style={styles.todoDescription}>{item.description}</Text>
-            </View>
-            <Pressable
-                style={[styles.checkbox, item.isComplete && styles.selectedCheckbox]}
-                onPress={() => {
-                    setComplete(!item.isComplete, item)
-                }}
-            >
-                <Text style={[styles.checkboxText, item.isComplete && styles.selectedCheckboxText]}>{(item.isComplete) ? "✓" : ""}</Text>
-            </Pressable>
-        </View >
+  async function setComplete(updateValue, todo) {
+    await DataStore.save(
+      Todo.copyOf(todo, updated => {
+        updated.isComplete = updateValue
+      })
     );
+  }
 
-    return (
-        <View style={styles.container}>
-            <FlatList
-                data={todos}
-                renderItem={todoItem}
-                keyExtractor={item => item.id}
-            />
-        </View>
-    );
+  const todoItem = ({ item }) => (
+    <View style={styles.todoContainer}>
+      <View>
+        <Text style={styles.todoHeading}>{item.name}</Text>
+        <Text style={styles.todoDescription}>{item.description}</Text>
+      </View>
+      <Pressable
+        style={[styles.checkbox, item.isComplete && styles.selectedCheckbox]}
+        onPress={() => {
+          setComplete(!item.isComplete, item)
+        }}
+      >
+        <Text style={[styles.checkboxText, item.isComplete && styles.selectedCheckboxText]}>{(item.isComplete) ? "✓" : ""}</Text>
+      </Pressable>
+    </View >
+  );
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={todos}
+        renderItem={todoItem}
+        keyExtractor={item => item.id}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    todoContainer: {
-        marginVertical: 5,
-        marginHorizontal: 10,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        backgroundColor: "white",
-        shadowColor: "#000000",
-        shadowOpacity: 0.3,
-        shadowRadius: 2,
-        shadowOffset: {
-            height: 1,
-            width: 1
-        }
-    },
-    todoHeading: {
-        fontSize: 20,
-        fontWeight: "600",
-        padding: 5,
-    },
-    todoDescription: {
-        padding: 5,
-    },
-    checkbox: {
-        alignSelf: "center",
-        alignItems: "center",
-        borderWidth: 2,
-        height: 20,
-        width: 20,
-        margin: 10,
-        borderRadius: 2,
-    },
-    checkboxText: {
-        fontSize: 13,
-        fontWeight: "700",
-        alignSelf: 'center',
-    },
-    selectedCheckbox: {
-        backgroundColor: "black"
-    },
-    selectedCheckboxText: {
-        color: "white"
-    },
+  container: {
+    flex: 1,
+  },
+  todoContainer: {
+    marginVertical: 5,
+    marginHorizontal: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "white",
+    shadowColor: "#000000",
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    shadowOffset: {
+      height: 1,
+      width: 1
+    }
+  },
+  todoHeading: {
+    fontSize: 20,
+    fontWeight: "600",
+    padding: 5,
+  },
+  todoDescription: {
+    padding: 5,
+  },
+  checkbox: {
+    alignSelf: "center",
+    alignItems: "center",
+    borderWidth: 2,
+    height: 20,
+    width: 20,
+    margin: 10,
+    borderRadius: 2,
+  },
+  checkboxText: {
+    fontSize: 13,
+    fontWeight: "700",
+    alignSelf: 'center',
+  },
+  selectedCheckbox: {
+    backgroundColor: "black"
+  },
+  selectedCheckboxText: {
+    color: "white"
+  },
 });
